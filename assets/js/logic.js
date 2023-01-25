9b4123c4989fba263e7d1044d5d816629e82be7b
bilginceliktas <114520003+bilginceliktas@users.noreply.github.com>
2023-01-25 01:28:08 +0000

    not finished yet

---
+++ assets/js/logic.js
@@ -0,0 +1,50 @@
+let currentQuestion = 0;
+let score = 0;
+let timer;
+
+document.getElementById("start-button").addEventListener("click", startQuiz);
+
+function startQuiz() {
+    timer = setInterval(() => {
+      const timerDisplay = document.getElementById("timer");
+      timerDisplay.innerText = parseInt(timerDisplay.innerText) + 1;
+      if (timerDisplay.innerText === "0") {
+        clearInterval(timer);
+        endQuiz();
+      }
+    }, 1000);
+    document.getElementById("start-screen").classList.add("hide");
+    document.getElementById("questions").classList.remove("hide");
+    displayQuestion();
+  }
+
+  function displayQuestion() {
+    if (currentQuestion === questions.length) {
+      endQuiz();
+      return;
+    }
+    const question = questions[currentQuestion];
+    document.getElementById("question-title").innerText = question.question;
+    const choices = document.getElementById("choices");
+    choices.innerHTML = "";
+    for (const answer of question.answers) {
+      const answerButton = document.createElement("button");
+      answerButton.innerText = answer;
+      answerButton.addEventListener("click", checkAnswer);
+      choices.appendChild(answerButton);
+    }
+  }
+  function checkAnswer(event) {
+    const answer = event.target.innerText;
+    if (answer === questions[currentQuestion].correctAnswer) {
+      alert("Correct!");
+      score++;
+    } else {
+      alert("Incorrect!");
+      const timerDisplay = document.getElementById("timer");
+      timerDisplay.innerText = parseInt(timerDisplay.innerText) - 10;
+    }
+    currentQuestion++;
+    displayQuestion();
+  }
+    
---
+++ assets/js/questions.js
@@ -0,0 +1,20 @@
+const questions = [
+  {
+    question: "What is the syntax for creating a variable in JavaScript?",
+    answers: [
+      "let x = 5",
+      "var x = 5",
+      "const x = 5"
+    ],
+    correctAnswer: "var x = 5"
+  },
+  {
+    question: "What is the syntax for an if statement in JavaScript?",
+    answers: [
+      "if x = 5",
+      "if (x == 5)",
+      "if x == 5"
+    ],
+    correctAnswer: "if (x == 5)"
+  }
+];
